9a2a538655966bcb5d146ae40c5eeefe5a778b29
Hung Dinh <dinhhcoding@gmail.com>
2020-11-10 20:26:26 -0500

    Inital Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world-angular';
+  title: string = 'My First Angular';
+  role: string = 'Student';
 }
+
